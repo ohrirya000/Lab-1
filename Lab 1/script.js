71fcfc5fa0cacfad71272c5c7ad02da361b0ed73
ohrirya000 <143513802+ohrirya000@users.noreply.github.com>
2023-12-06 09:50:07 -0500

    Add files via upload

---
+++ Lab 1/script.js
@@ -0,0 +1,15 @@
+function changeColor(squareId){
+    var square = document.getElementById(squareId);
+    var randomColor = getRandomColor();
+    square.style.backgroundColor = randomColor;
+}
+
+
+function getRandomColor(){
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
